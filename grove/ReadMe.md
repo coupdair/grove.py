aac7e90d90dce02dcf81e65c86389e415d4d6135
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2021-03-23 09:34:08 +0000

    temperature: add CLI arg. parse with -n option for number of value to read

---
+++ grove/ReadMe.md
@@ -0,0 +1,10 @@
+# temperature
+
+~~~ { .bash }
+#help
+./grove_high_accuracy_temperature.py --help
+#get temperature 3 times
+./grove_high_accuracy_temperature.py -n 3
+#get temperature until Ctrl+C pressed
+./grove_high_accuracy_temperature.py
+~~~
--- grove/grove_high_accuracy_temperature.py
+++ grove/grove_high_accuracy_temperature.py
@@ -33,23 +33,60 @@ import time
 from grove.factory import Factory
 from grove.temperature import Temper
 
+import argparse
+
+#metadata
+__version__     = "0.0.1"
+__author__      = "Sebastien COUDERT"
+
 __all__ = ['Temper']
 
 def main():
-    print("Insert Grove - I2C-High-Accuracy-Temperature")
-    print("  to Grove-Base-Hat any I2C slot")
+  #CLI option
+  parser = argparse.ArgumentParser(description='get temperature on Grove High Accuracy device on I2C bus'
+  + ' (%(prog)s v'+__version__+')'
+  , epilog="examples: %(prog)s --help"
+  )
+  ##version
+  parser.add_argument('-v', '--version'
+  , action="store_true", default=False
+  , help='show version (default: False) currently v'+__version__)
+  ##integer
+  parser.add_argument('-n', '--number'
+  , default=-2
+  , help='number of iteration')
 
-    sensor = Factory.getTemper("MCP9808-I2C")
-    sensor.resolution(Temper.RES_1_16_CELSIUS)
+  ##parse
+  args=parser.parse_args()
+  version=args.version
+  nb=int(args.number)+1
+  #}CLI option
 
-    print("resolution=",sensor._resolution)
+  if(version):
+    print __version__
+    quit()
 
-    print('Detecting temperature...')
+
+
+  print("Insert Grove - I2C-High-Accuracy-Temperature")
+  print("  to Grove-Base-Hat any I2C slot")
+
+  sensor = Factory.getTemper("MCP9808-I2C")
+  sensor.resolution(Temper.RES_1_16_CELSIUS)
+
+  print("resolution=",sensor._resolution)
+
+  print('Detecting temperature...')
+  if(nb<0):
     while True:
-        print('{} Celsius'.format(sensor.temperature))
-        time.sleep(1)
-
+      print('{} Celsius'.format(sensor.temperature))
+      time.sleep(1)
+    #}endless loop
+  else:
+    for i in range(1,nb):
+      print('{} Celsius'.format(sensor.temperature))
+      time.sleep(1)
+    #}i loop
 
 if __name__ == '__main__':
     main()
-
